b000e5003707c32a3bf80c495364129e438be198
Karyna Kachanovska <kkc5fe@bosch.com>
2025-01-14 16:09:49 +0100

    Initialize project

---
+++ src/main/kotlin/kachanovska/playground/spring_boot_demo/SpringBootDemoApplication.kt
@@ -0,0 +1,11 @@
+package kachanovska.playground.spring_boot_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBootDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBootDemoApplication>(*args)
+}
---
+++ src/test/kotlin/kachanovska/playground/spring_boot_demo/SpringBootDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package kachanovska.playground.spring_boot_demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
